94c272268fda4ecb07d227549ff6d6be1a21d616
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-07-03 06:52:28 +1000

    Updated the nav bar

--- resources/views/layouts/www/navigation.blade.php
+++ resources/views/layouts/www/navigation.blade.php
@@ -1,13 +1,22 @@
 <div>
   <ul id="navigation_bar">
     <li>
-      <a style="padding: 16px 0 0 0;" href="/">Home</a>
+      <a style="padding: 8px;" href="/">Home</a>
     </li>
     <li>
-      <a style="padding: 16px 0 0 0;" href="/About">About Me</a>
+      <a style="padding: 8px;" href="/About">About Me</a>
     </li>
-    <li style="height: 36px">
-      <a href="https://github.com/Hiroki111" target="_blank"><img style="height: 36px" src="/storage/template/github.png"></a>
+    <li style="height: 36px;">
+      <a style="padding: 0;" href="https://github.com/Hiroki111" target="_blank">
+        <img style="height: 36px; padding:8px;" src="/storage/template/github-icon.png">
+        GitHub
+      </a>
+    </li>
+    <li style="height: 36px;">
+      <a style="padding: 0;" href="https://www.linkedin.com/in/hiroki-takahashi-198986b1/" target="_blank">
+        <img style="height: 36px; padding:8px;" src="/storage/template/linkedin.png">
+        LinkedIn
+      </a>
     </li>
   </ul>
 </div>
